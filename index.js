30706a4d2e057e375cf6fc5b94c7e26cab9817e8
Derrick Momanyi <derrick.momanyi@student.moringaschool.com>
2022-12-08 23:53:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-  for (let i = 0; i<array.length; i++) {
-    const complement = target - array[i];
-    for (let j = i + 1; j < array.length; j++) {
-      if(array[j] > complement) {
-        return true;
+  for (let i = 0; i<array.length; i++) {      // iterates through the array calculating the complement 
+    const complement = target - array[i];    // for every element in the array calculate the complement
+    for (let j = i + 1; j < array.length; j++) {  // the next iteration loopss through the array finding if our complement exists
+      if(array[j] === complement) {
+        return true;                       //if the complement exists return true otherwise  return false
       }
     }
   }
@@ -13,16 +13,29 @@ function hasTargetSum(array, target) {
 
 /* 
   Write the Big O time complexity of your function here
-*/
+  loop 1 = n steps  
+  loop 2 (nested ) = n*n steps
+  Time complexity = O(n^2)
+  Space complexity = O(n)
+ */
 
 /* 
   Add your pseudocode here
+  hasTargetSum([3, 8, 12, 4, 11, 7], 10));
+  iterate through each number in the array 
+  for the correct number identify a number that adds to the target
+  the correct number is target - number  
 */
 
 /*
   Add written explanation of your solution here
-*/
+  Had to check whether any 2 numbers in the array add up to the target. 
+If an array includes [4,5,6,7] and the target is 11, it should return true. Otherwise it should return false
+Created a function hasTargetSum that take in an array and a target
+Wrote the first iteration of the array using it
 
+*/
+ 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
@@ -41,3 +54,4 @@ if (require.main === module) {
 }
 
 module.exports = hasTargetSum;
+
